d729113435af185d5e363b07363e8a19b4f3f1ad
AnastasiaZhukova <azhuk.mail@gmail.com>
2017-10-04 20:40:49 +0300

    Added new method to MainActivity

--- app/src/main/java/com/github/anastasiazhukova/playingwithgihub/MainActivity.java
+++ app/src/main/java/com/github/anastasiazhukova/playingwithgihub/MainActivity.java
@@ -10,4 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void SomeMethod()
+    {
+        //Creating new method from master Branch
+    }
 }
